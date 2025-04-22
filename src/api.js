37a22bc1c7ec55ad60bdb93979854d9b0de8768f
sushant2077 <budhathoki\sushant2077@gmail.com>
2025-04-23 09:36:41 +1000

    final

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-const endpoint = "https://d0fe-2403-4800-2909-3901-ad19-6de1-c304-7fae.ngrok-free.app/api";
+const endpoint = "https://7d39-119-18-6-133.ngrok-free.app/api";
 export default endpoint;
